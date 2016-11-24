0779eadaebed4d327b4e5c4f44b488fc10416abf
Ian Sinnott <ian@iansinnott.com>
2016-11-24 15:01:30 -0800

    Refactor base config into separate file

---
+++ base.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['eslint:recommended'],
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  plugins: [],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+    },
+  },
+  rules: {
+    'indent': [2, 2],
+    'no-console': [0],
+    'linebreak-style': [2, 'unix'],
+    'quotes': [2, 'single'],
+    'semi': [1, 'always'],
+    'no-unused-vars': [1, { vars: 'all', args: 'none' }],
+  },
+};
